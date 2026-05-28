0d22a3e4fd228a9a80711e600a015bee7ccf1765
Piotr Lichota <153811691+piotr-lichota342@users.noreply.github.com>
2026-05-28 14:50:18 +0200

    Added exponential lr

--- code/config.py
+++ code/config.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 N_spins = 12
 BATCH_SIZE = 40
-W = 8192
+W = 4096
 EPOCHS = int(os.environ.get("EPOCHS", 3))
 HIDDEN_LAYERS = int(os.environ.get("HIDDEN_LAYERS", 2))
 INPUT_SIZE = (32,12)
@@ -16,6 +16,7 @@ TEST_PROPORTION = 0.2
 VALID_PROPORTION = 0.1
 DATASET_SIZE = 4096
 TIMESTAMP = str(os.environ.get("TIMESTAMP", None))
+EXPONENTIAL_LR = True
 
 trained_regimes = os.environ.get("trained_regimes", None)
 
