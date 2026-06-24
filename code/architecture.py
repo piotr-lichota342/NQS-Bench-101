8810d0a0dd1f2c7ee1c4fb4ad9d31b8326380cb6
Piotr Lichota <153811691+piotr-lichota342@users.noreply.github.com>
2026-06-24 21:57:14 +0200

    Current architecture.py

--- code/architecture.py
+++ code/architecture.py
@@ -7,8 +7,11 @@ import pandas as pd
 from torch.utils.data import random_split
 from torch.nn import Parameter
 from torchinfo import summary
+from config import WIDTH_SEQUENCE
 
-from config import N_spins, W, device, HIDDEN_LAYERS, INPUT_SIZE, ACT_FUNCTION
+from config import N_spins, W, device, HIDDEN_LAYERS, INPUT_SIZE, ACT_FUNCTION, CUSTOM_ARCH, WIDTH_SEQUENCE
+
+
 
 activation_f = None
 
@@ -24,26 +27,45 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         #self.flatten = nn.Flatten()
-        layers = []
-        layers.append(nn.Linear(N_spins, W))
-        layers.append(nn.LayerNorm(W))
-        layers.append(activation_f)
-        
-        '''layers.append(nn.Linear(W, 512))
-        layers.append(nn.LayerNorm(512))
-        layers.append(activation_f)'''
-        
-        
-        for hl in range(HIDDEN_LAYERS-1):
-            layers.append(nn.Linear(W, W))
+        if not CUSTOM_ARCH:
+            layers = []
+            layers.append(nn.Linear(N_spins, W))
             layers.append(nn.LayerNorm(W))
             layers.append(activation_f)
             
-        layers.append(nn.Linear(W, 1))
-        
-        self.linear_gelu_stack = nn.Sequential(
-            *layers
-        )
+            '''layers.append(nn.Linear(W, 512))
+            layers.append(nn.LayerNorm(512))
+            layers.append(activation_f)'''
+            
+            
+            for hl in range(HIDDEN_LAYERS):
+                layers.append(nn.Linear(W, W))
+                layers.append(nn.LayerNorm(W))
+                layers.append(activation_f)
+                
+            layers.append(nn.Linear(W, 1))
+            
+            self.linear_gelu_stack = nn.Sequential(
+                *layers
+            )
+        else:
+            layers = []
+            width_sequence = WIDTH_SEQUENCE.split(",")
+            width_sequence = [int(w) for w in width_sequence]
+            layers.append(nn.Linear(N_spins, width_sequence[0]))
+            layers.append(nn.LayerNorm(width_sequence[0]))
+            layers.append(activation_f)
+            
+            for hl in range(0,len(width_sequence)-1):
+                layers.append(nn.Linear(width_sequence[hl], width_sequence[hl+1]))
+                layers.append(nn.LayerNorm(width_sequence[hl+1]))
+                layers.append(activation_f)
+                
+            layers.append(nn.Linear(width_sequence[-1], 1))
+            
+            self.linear_gelu_stack = nn.Sequential(
+                *layers
+            )
         
 
     def forward(self, x):
